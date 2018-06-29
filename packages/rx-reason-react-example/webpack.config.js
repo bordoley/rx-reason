57aaf45d4544c12cefba4367888cd54b759fe7e7
David Bordoley <bordoley@gmail.com>
2018-06-29 13:57:51 -0400

    build es6 modules to get webpack benefits.

--- packages/rx-reason-react-example/webpack.config.js
+++ packages/rx-reason-react-example/webpack.config.js
@@ -4,7 +4,7 @@ const outputDir = path.join(__dirname, "build/");
 const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  entry: './lib/js/src/Index.bs.js',
+  entry: './lib/es6/src/Index.bs.js',
   mode: isProd ? 'production' : 'development',
   output: {
     path: outputDir,
